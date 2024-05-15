2666a4e934ff3fbe5bdbe661a73739faa48cb2de
Nikolay <tehlic@gmail.com>
2024-05-15 10:37:14 +0400

    feat: New beta with quave:synced-cron

--- example/packages/oplog-keep-awake/package.js
+++ example/packages/oplog-keep-awake/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'kolyasya:oplog-keep-awake',
-  version: '0.1.0-beta.0',
+  version: '0.1.0-beta.1',
   summary:
     'Upserts a new entry into keepAwake collection to keep an oplog tailable',
   git: 'https://github.com/kolyasya/meteor-oplog-keep-awake',
@@ -10,8 +10,8 @@ Package.describe({
 Npm.depends({});
 
 Package.onUse(function (api) {
-  api.versionsFrom('2.8.0');
-  api.use(['ecmascript@0.16.3', 'littledata:synced-cron@1.5.1']);
+  api.versionsFrom('3.0-rc.1');
+  api.use(['ecmascript@0.16.3', 'quave:synced-cron@2.0.3']);
 
   api.mainModule('server.js', 'server');
 });
